d12c4de8ee0c62238add67b49c9175f4bfa19dee
Radu Pavel <radu4313@gmail.com>
2022-09-20 22:57:44 +0300

    Spring project initialisation

---
+++ src/main/java/com/ing/testapp/IngSpringTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ing.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngSpringTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngSpringTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ing/testapp/IngSpringTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.testapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngSpringTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
